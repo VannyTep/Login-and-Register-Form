b3772579f17fa377161a6799257404b68cc04be4
VannyTep <89964001+VannyTep@users.noreply.github.com>
2023-12-11 20:34:30 +0700

    I have made a login form

---
+++ client/src/pages/LoginForm.jsx
@@ -0,0 +1,80 @@
+import { useState } from "react";
+import "../styles/RegisterForm.css";
+import RegisterValidation from "../js/registerValidation";
+
+function RegisterForm() {
+  const [values, setValues] = useState({
+    username: "",
+    email: "",
+    password: "",
+    confirmed_password: "",
+  });
+
+  const [errors, setErrors] = useState({});
+
+  const handleInput = (e) => {
+    setValues((prev) => ({
+      ...prev,
+      [event.target.name]: [event.target.value],
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setErrors(RegisterValidation(values));
+  };
+
+  return (
+    <div className="container">
+      <form action="" onSubmit={handleSubmit}>
+        <h1>Register</h1>
+        <div className="username-input-container">
+          <input
+            type="text"
+            name="username"
+            id="username-input"
+            placeholder="Username"
+            onChange={handleInput}
+          />
+          <span id="errors-username">
+            {errors.username ? errors.username : "U"}
+          </span>
+        </div>
+
+        <div className="email-input-container">
+          <input
+            type="email"
+            name="email"
+            id="email-input"
+            placeholder="Email"
+            onChange={handleInput}
+          />
+          <span id="errors-email">
+            {errors.username ? errors.username : "E"}
+          </span>
+        </div>
+
+        <div className="password-input-container">
+          <input
+            type="password"
+            name="password"
+            id="password-input"
+            placeholder="Password"
+            onChange={handleInput}
+          />
+          <span id="errors-email">
+            {errors.password ? errors.password : "P"}
+          </span>
+        </div>
+
+        <p id="having-acc-txt">
+          Already have an account? <a href="">Login Here</a>
+        </p>
+
+        <input type="submit" value="Login" id="submit-btn" />
+      </form>
+    </div>
+  );
+}
+
+export default RegisterForm;
