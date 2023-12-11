0ab5810fbbaefc3b3518b3c154f74a4811e7aeab
TepChoulong <tepchoulongzin@gmail.com>
2023-12-11 21:35:56 +0700

    Add link to home

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,5 +1,12 @@
+import { Link } from "react-router-dom";
+
 function Home() {
-  return <h1>Welcome to Home Page</h1>;
+  return (
+    <>
+      <h1>Welcome to Home Page</h1>
+      <Link to="/register">Register</Link>
+    </>
+  );
 }
 
 export default Home;
